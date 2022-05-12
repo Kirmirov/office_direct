29f223897dd87b30cebb36c85678ba93f167d78f
Kirmirov <aruter@yandex.ru>
2022-05-12 18:09:21 +0300

    add map

--- src/js/main.js
+++ src/js/main.js
@@ -44,3 +44,75 @@ const selectControl = (selectID) =>
 
 
 selectControl('#select1');
+
+const init = () => {
+	let map = new ymaps.Map("map", {
+		center: [43.20259659959205,27.9168840397141],
+		zoom: 15
+	});
+
+	const officeCenter 	= [43.200320445995864,27.91386387236333];
+	const seeCenter 	= [43.1950286176228,27.917849134096397];
+	const parkCenter 	= [43.202902178793,27.9231500132146];
+
+	let officeMark = new ymaps.Placemark(officeCenter, 
+	{
+		balloonContentHeader: "OфисDирект",
+		balloonContentBody: "Режим работы: c 8:00 до 16:00",
+		balloonContentFooter: "Варна, ул. Козлодуй, блок 3, ап. 10"
+	}, 
+	{
+		iconLayout: "default#image",
+		iconImageHref: "../img/map/office.png",
+		iconImageSize: [99, 99],
+		iconImageOffset: [-50, -100]
+	});
+
+	map.geoObjects.add(officeMark);
+	officeMark.balloon.open();
+
+	let seeMark = new ymaps.Placemark(seeCenter, 
+	{
+		balloonContentHeader: "Морской порт",
+		balloonContentBody: "Режим работы: c 10:00 до 20:00",
+		balloonContentFooter: "Варна, Приморский бульвар"
+	}, 
+	{
+		preset: 'islands#darkOrangeIcon',
+		iconImageSize: [99, 99],
+		iconImageOffset: [-50, -150]
+	});
+
+	map.geoObjects.add(seeMark);
+	seeMark.balloon.open();
+
+	let parkMark = new ymaps.Placemark(parkCenter, 
+	{
+		balloonContentHeader: "Городской парк",
+		balloonContentBody: "Режим работы: круглосуточно",
+		balloonContentFooter: "Варна, бульвар Князя Бориса I"
+	}, 
+	{
+		preset: 'islands#violetCircleIcon',
+		iconImageSize: [99, 99],
+		iconImageOffset: [50, 100]
+	});
+
+	map.geoObjects.add(parkMark);
+	parkMark.balloon.open();
+
+
+
+	map.controls.remove('geolocationControl');
+	map.controls.remove('searchControl');
+	map.controls.remove('trafficControl');
+	map.controls.remove('typeSelector');
+	map.controls.remove('fullscreenControl');
+	map.controls.remove('zoomControl');
+	map.controls.remove('rulerControl');
+	map.behaviors.disable(['scrollZoom']);
+
+}
+
+ymaps.ready(init);
+	
